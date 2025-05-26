ebc3330075a8e2afea77e840aea586f0b28ad503
RynJyy <ryancanlas299@gmail.com>
2025-05-26 20:22:38 +0800

    create main class and main loop

--- quiz_creator.py
+++ quiz_creator.py
@@ -2,3 +2,19 @@ from quiz_creator_ui import HeaderDisplay
 from quiz_creator_questions import QuizQuestion
 from quiz_creator_storage import QuizStorage
 from colorama import Fore
+
+class QuizCreator:
+    def __init__(self):
+        self.storage = QuizStorage()
+
+    def run(self):
+        HeaderDisplay.show()
+        while True:
+            question = QuizQuestion()
+            question.get_from_user()
+            self.storage.save_question(question.to_dict())
+
+            another = input(Fore.YELLOW + "Do you want to add another question? (yes/no): ").lower()
+            if another != 'yes':
+                print(Fore.BLUE + "Thank you for using the quiz creator, your file is now saved!")
+                break
