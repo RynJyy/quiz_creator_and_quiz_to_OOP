f3d1197bb88ff17c6409cd796602e5aa87321449
RynJyy <ryancanlas299@gmail.com>
2025-05-26 20:07:35 +0800

    def question details

--- question.py
+++ question.py
@@ -7,5 +7,29 @@ class QuizQuestion:
         self.correct_answer = ""
         self.category = ""
         self.difficulty = ""
+    def get_from_user(self):
+            print(Fore.YELLOW + "\nEnter a new quiz question:")
+            self.question = input(Fore.GREEN + "Question: ")
 
-    
+            self.answers = []
+            for option in ['a', 'b', 'c', 'd']:
+                answer = input(Fore.GREEN + f"Option {option}: ")
+                self.answers.append(answer)
+
+            while True:
+                self.correct_answer = input(Fore.GREEN + "Correct answer (a/b/c/d): ").lower()
+                if self.correct_answer in ['a', 'b', 'c', 'd']:
+                    break
+                else:
+                    print(Fore.RED + "Invalid input. Please enter a, b, c, or d.")
+
+            self.category = input(Fore.GREEN + "Category: ")
+
+            while True:
+                self.difficulty = input(Fore.GREEN + "Difficulty (intro/beginner/intermediate): ").lower()
+                if self.difficulty in ['intro', 'beginner', 'intermediate']:
+                    break
+                else:
+                    print(Fore.RED + "Invalid input. Please enter intro, beginner, or intermediate.")
+
+        
