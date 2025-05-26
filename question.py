9c8fc44d78ab8c9b990a6fe99048457d6347ad42
RynJyy <ryancanlas299@gmail.com>
2025-05-26 20:06:04 +0800

    add question class

---
+++ question.py
@@ -0,0 +1,11 @@
+from colorama import Fore
+
+class QuizQuestion:
+    def __init__(self):
+        self.question = ""
+        self.answers = []
+        self.correct_answer = ""
+        self.category = ""
+        self.difficulty = ""
+
+    
