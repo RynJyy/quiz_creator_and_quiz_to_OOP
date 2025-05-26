8aa3819f65271eeb1c7ac07de94c977b566c839b
RynJyy <ryancanlas299@gmail.com>
2025-05-26 20:31:09 +0800

    fix class name error

--- quiz_creator_ui.py
+++ quiz_creator_ui.py
@@ -1,10 +1,11 @@
-from colorama import Fore, init
+from colorama import Fore
 
-init(autoreset=True)
+class UI:
+    @staticmethod
+    def show():
+        print(Fore.BLUE + "=" * 34)
+        print(Fore.MAGENTA + "📚 Welcome to the quiz creator! 📚")
+        print(Fore.BLUE + "=" * 34 + "\n")
+        print(Fore.CYAN + "This program allows you to create your own quiz questions.")
+        print(Fore.CYAN + "You can add questions, specify answers, and save them for later use.\n")
 
-def show_header():
-    print(Fore.BLUE + "=" * 34)
-    print(Fore.MAGENTA + "📚 Welcome to the quiz creator! 📚")
-    print(Fore.BLUE + "=" * 34 + "\n")
-    print(Fore.CYAN + "This program allows you to create your own quiz questions.")
-    print(Fore.CYAN + "You can add questions, specify answers, and save them for later use.\n")
