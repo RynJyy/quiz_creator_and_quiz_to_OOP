229c1ba4566cb05d96d23682c6db24d00f01867d
RynJyy <ryancanlas299@gmail.com>
2025-05-27 21:35:38 +0800

    added comments

--- quiz_creator_storage.py
+++ quiz_creator_storage.py
@@ -3,15 +3,19 @@ from colorama import Fore
 
 class QuizStorage:
     def __init__(self, filename='quiz_questions.json'):
+        # Initialize with JSON filename
         self.filename = filename
 
     def load_data(self):
+        # Load quiz data from JSON file
         try:
             with open(self.filename, 'r') as file:
                 return json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             return []
+
     def save_question(self, question_data):
+        # Append new question and save to file
         data = self.load_data()
         data.append(question_data)
         with open(self.filename, 'w') as file:
