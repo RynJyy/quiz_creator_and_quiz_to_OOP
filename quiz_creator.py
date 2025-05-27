a3ae2a803b0737d4fd6abcd4c80f242f80650cdf
RynJyy <ryancanlas299@gmail.com>
2025-05-27 21:37:20 +0800

    added cpmments

--- quiz_creator.py
+++ quiz_creator.py
@@ -5,16 +5,16 @@ from colorama import Fore
 
 class QuizCreator:
     def __init__(self):
-        self.storage = QuizStorage()
+        self.storage = QuizStorage()  # Initialize storage handler
 
     def run(self):
-        UI.show()
+        UI.show()  # Display welcome message and instructions
         while True:
             question = QuizQuestion()
-            question.get_from_user()
-            self.storage.save_question(question.to_dict())
+            question.get_from_user()  # Prompt user to input question details
+            self.storage.save_question(question.to_dict())  # Save question to JSON
 
             another = input(Fore.YELLOW + "Do you want to add another question? (yes/no): ").lower()
             if another != 'yes':
                 print(Fore.BLUE + "Thank you for using the quiz creator, your file is now saved!")
-                break
+                break  # Exit loop if user doesn’t want to add more questions
