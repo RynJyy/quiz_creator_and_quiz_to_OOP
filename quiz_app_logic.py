a31d164c851a84b25046b97b5b762ba28681b901
RynJyy <ryancanlas299@gmail.com>
2025-05-27 20:23:08 +0800

    def random questions and compare answers

--- quiz_app_logic.py
+++ quiz_app_logic.py
@@ -8,4 +8,14 @@ class QuizLogic:
         self.total_questions = len(quiz_data)
         self.current_question = None
 
-    
+    def next_question(self):
+        self.current_question = random.choice(self.quiz_data)
+        return self.current_question
+
+    def check_answer(self, answer):
+        correct = self.current_question['correct_answer']
+        is_correct = answer == correct
+        if is_correct:
+            self.score += 1
+        self.questions_asked += 1
+        return is_correct, correct
