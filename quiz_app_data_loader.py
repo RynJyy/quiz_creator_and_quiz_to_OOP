1cbfcd81ee2390542dd1f59b76979c1d42ea5ede
RynJyy <ryancanlas299@gmail.com>
2025-05-27 21:31:31 +0800

    added comments

--- quiz_app_data_loader.py
+++ quiz_app_data_loader.py
@@ -3,12 +3,15 @@ from tkinter import messagebox
 
 class QuizDataLoader:
     def __init__(self, filename='quiz_questions.json'):
+        # Set the JSON file path
         self.filename = filename
 
     def load_data(self):
+        # Load quiz data from JSON file
         try:
             with open(self.filename, 'r') as file:
                 return json.load(file)
         except FileNotFoundError:
+            # Show error if file not found and return empty list
             messagebox.showerror("Error", "No questions found.")
             return []
