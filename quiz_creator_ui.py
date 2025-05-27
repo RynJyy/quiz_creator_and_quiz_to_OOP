e23f1bbcf445cb84773cddd587e6bf4240576245
RynJyy <ryancanlas299@gmail.com>
2025-05-27 21:36:45 +0800

    added comments

--- quiz_creator_ui.py
+++ quiz_creator_ui.py
@@ -3,8 +3,9 @@ from colorama import Fore
 class UI:
     @staticmethod
     def show():
+        # Print welcome header and instructions with color styling
         print(Fore.BLUE + "=" * 34)
-        print(Fore.MAGENTA + "📚 Welcome to the quiz creator! 📚")
+        print(Fore.MAGENTA + "Welcome to the quiz creator!")
         print(Fore.BLUE + "=" * 34 + "\n")
         print(Fore.CYAN + "This program allows you to create your own quiz questions.")
         print(Fore.CYAN + "You can add questions, specify answers, and save them for later use.\n")
